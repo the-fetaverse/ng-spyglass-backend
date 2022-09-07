c29f95af54b82015d0d1c0d747d8dd18b26cc15f
the-fetaverse <ioannis.sporidis@gmail.com>
2022-09-07 14:02:28 -0400

    Registration and user controllers ready

--- src/main/java/com/fetaverse/ngspyglassbackend/controllers/RegistrationController.java
+++ src/main/java/com/fetaverse/ngspyglassbackend/controllers/RegistrationController.java
@@ -3,10 +3,12 @@ package com.fetaverse.ngspyglassbackend.controllers;
 import com.fetaverse.ngspyglassbackend.DTO.UserDTO;
 import com.fetaverse.ngspyglassbackend.services.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class RegistrationController {
 
--- src/main/java/com/fetaverse/ngspyglassbackend/services/GoalService.java
+++ src/main/java/com/fetaverse/ngspyglassbackend/services/GoalService.java
@@ -3,6 +3,7 @@ package com.fetaverse.ngspyglassbackend.services;
 import com.fetaverse.ngspyglassbackend.models.Goal;
 import com.fetaverse.ngspyglassbackend.repositories.GoalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,6 +14,10 @@ public class GoalService {
 
     @Autowired
     private GoalRepository repository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
 
     public List<Goal> findByUsername(String username) {
         return repository.findByUsername(username);
